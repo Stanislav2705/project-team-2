dab530f0cd7599a1fab328cb7bff355de2907de5
Stanislav2705 <stasrogal25@gmail.com>
2022-08-30 00:23:04 +0300

    add

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import {
   modalCardRef,
   mainRefs,
 } from './js/refs';
-import { fetchFilm } from './js/fetch-film';
+// import { fetchFilm } from './js/fetch-film';
 import { generateContentGallery } from './js/markup-list';
 import { createGallaryHome } from './js/create-gallary';
 import { toggleModal } from './js/modal';
+import { main } from './js/pagination';
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,1 +1,29 @@
+import axios from 'axios';
+import { generateContentGallery } from '../js/markup-list';
+import { fetchMovie, fetchGenre, searchMovieByKey } from '../js/fetch-film';
+let PAGE_SIZE = 20;
+let currentPage = 1;
 
+export async function main() {
+  function displayPagination(arrData, rowPerPage) {
+    const paginationEl = document.querySelector('.pagination');
+    const pagesCount = Math.ceil(arrData.length / rowPerPage);
+    const ulEl = document.createElement('ul');
+    ulEl.classList.add('pagination__list');
+
+    for (let i = 0; i < pagesCount; i++) {
+      const liEl = displayPaginationBtn(i + 1);
+      ulEl.appendChild(liEl);
+    }
+    paginationEl.appendChild(ulEl);
+  }
+
+  function displayPaginationBtn(page) {
+    const liEl = document.createElement('li');
+    liEl.classList.add('pagination__item');
+    liEl.innerText = page;
+    return liEl;
+  }
+
+  displayPagination(generateContentGallery, PAGE_SIZE);
+}
