0b4d0ecd78a91e5087d26b741b4c5d745700ee3d
Stanislav2705 <stasrogal25@gmail.com>
2022-10-03 21:21:22 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,105 +1,105 @@
 import './sass/main.scss';
 import './js/preloader';
-// import { generateContentGallery } from './js/markup-list';
-// import { createGallaryHome } from './js/create-gallary';
-// import { toggleModal } from './js/modal';
-import { PAGE_SIZE, currentPage } from './js/fetch-film';
+import { generateContentGallery } from './js/markup-list';
+import { createGallaryHome } from './js/create-gallary';
+import { toggleModal } from './js/modal';
+// import { PAGE_SIZE, currentPage } from './js/fetch-film';
 
-const pageCount = 5;
+// const pageCount = 5;
 
-const paginationNumbers = document.getElementById('pagination-numbers');
-const paginatedList = document.getElementById('.paginated-list');
-const listItems = document.querySelectorAll('.section-gallery__item');
-const nextButton = document.getElementById('next-button');
-const prevButton = document.getElementById('prev-button');
+// const paginationNumbers = document.getElementById('pagination-numbers');
+// const paginatedList = document.getElementById('.paginated-list');
+// const listItems = document.querySelectorAll('.section-gallery__item');
+// const nextButton = document.getElementById('next-button');
+// const prevButton = document.getElementById('prev-button');
 
-const disableButton = button => {
-  button.classList.add('disabled');
-  button.setAttribute('disabled', true);
-};
+// const disableButton = button => {
+//   button.classList.add('disabled');
+//   button.setAttribute('disabled', true);
+// };
 
-const enableButton = button => {
-  button.classList.remove('disabled');
-  button.removeAttribute('disabled');
-};
+// const enableButton = button => {
+//   button.classList.remove('disabled');
+//   button.removeAttribute('disabled');
+// };
 
-const handlePageButtonsStatus = () => {
-  if (currentPage === 1) {
-    disableButton(prevButton);
-  } else {
-    enableButton(prevButton);
-  }
+// const handlePageButtonsStatus = () => {
+//   if (currentPage === 1) {
+//     disableButton(prevButton);
+//   } else {
+//     enableButton(prevButton);
+//   }
 
-  if (pageCount === currentPage) {
-    disableButton(nextButton);
-  } else {
-    enableButton(nextButton);
-  }
-};
+//   if (pageCount === currentPage) {
+//     disableButton(nextButton);
+//   } else {
+//     enableButton(nextButton);
+//   }
+// };
 
-const handleActivePageNumber = () => {
-  document.querySelectorAll('.pagination-number').forEach(button => {
-    button.classList.remove('active');
+// const handleActivePageNumber = () => {
+//   document.querySelectorAll('.pagination-number').forEach(button => {
+//     button.classList.remove('active');
 
-    const pageIndex = Number(button.getAttribute('page-index'));
-    if (pageIndex == currentPage) {
-      button.classList.add('active');
-    }
-  });
-};
+//     const pageIndex = Number(button.getAttribute('page-index'));
+//     if (pageIndex == currentPage) {
+//       button.classList.add('active');
+//     }
+//   });
+// };
 
-const appendPageNumber = index => {
-  const pageNumber = document.createElement('button');
-  pageNumber.className = 'pagination-number';
-  pageNumber.innerHTML = index;
-  pageNumber.setAttribute('page-index', index);
-  pageNumber.setAttribute('aria-label', 'Page ' + index);
+// const appendPageNumber = index => {
+//   const pageNumber = document.createElement('button');
+//   pageNumber.className = 'pagination-number';
+//   pageNumber.innerHTML = index;
+//   pageNumber.setAttribute('page-index', index);
+//   pageNumber.setAttribute('aria-label', 'Page ' + index);
 
-  paginationNumbers.appendChild(pageNumber);
-};
+//   paginationNumbers.appendChild(pageNumber);
+// };
 
-const getPaginationNumbers = () => {
-  for (let i = 1; i <= pageCount; i++) {
-    appendPageNumber(i);
-  }
-};
+// const getPaginationNumbers = () => {
+//   for (let i = 1; i <= pageCount; i++) {
+//     appendPageNumber(i);
+//   }
+// };
 
-const setCurrentPage = pageNum => {
-  currentPage = pageNum;
+// const setCurrentPage = pageNum => {
+//   currentPage = pageNum;
 
-  handleActivePageNumber();
-  handlePageButtonsStatus();
+//   handleActivePageNumber();
+//   handlePageButtonsStatus();
 
-  const prevRange = (pageNum - 1) * PAGE_SIZE;
-  const currRange = pageNum * PAGE_SIZE;
+//   const prevRange = (pageNum - 1) * PAGE_SIZE;
+//   const currRange = pageNum * PAGE_SIZE;
 
-  listItems.forEach((item, index) => {
-    item.classList.add('hidden');
-    if (index >= prevRange && index < currRange) {
-      item.classList.remove('hidden');
-    }
-  });
-};
+//   listItems.forEach((item, index) => {
+//     item.classList.add('hidden');
+//     if (index >= prevRange && index < currRange) {
+//       item.classList.remove('hidden');
+//     }
+//   });
+// };
 
-window.addEventListener('load', () => {
-  getPaginationNumbers();
-  setCurrentPage(1);
+// window.addEventListener('load', () => {
+//   getPaginationNumbers();
+//   setCurrentPage(1);
 
-  prevButton.addEventListener('click', () => {
-    setCurrentPage(currentPage - 1);
-  });
+//   prevButton.addEventListener('click', () => {
+//     setCurrentPage(currentPage - 1);
+//   });
 
-  nextButton.addEventListener('click', () => {
-    setCurrentPage(currentPage + 1);
-  });
+//   nextButton.addEventListener('click', () => {
+//     setCurrentPage(currentPage + 1);
+//   });
 
-  document.querySelectorAll('.pagination-number').forEach(button => {
-    const pageIndex = Number(button.getAttribute('page-index'));
+//   document.querySelectorAll('.pagination-number').forEach(button => {
+//     const pageIndex = Number(button.getAttribute('page-index'));
 
-    if (pageIndex) {
-      button.addEventListener('click', () => {
-        setCurrentPage(pageIndex);
-      });
-    }
-  });
-});
+//     if (pageIndex) {
+//       button.addEventListener('click', () => {
+//         setCurrentPage(pageIndex);
+//       });
+//     }
+//   });
+// });
