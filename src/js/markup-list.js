d4c0bc40bad3f4c73c22e803b697189db1625915
Stanislav2705 <stasrogal25@gmail.com>
2023-07-24 22:01:57 +0200

    library list

--- src/js/markup-list.js
+++ src/js/markup-list.js
@@ -13,7 +13,7 @@ function createGallary({
   const release = release_date
     ? release_date.slice(0, 4)
     : 'Невідомо дати релізу';
-  const markup = `<li class="section-gallery__item paginated-list" id="${id}">
+  const markup = `<li class="${listItemClass}" id="${id}">
           <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${original_title}" id="${id}" width="280" class="section-gallery__item__img">
           <h2 class="section-gallery__item__title" id="${id}">${title}</h2>
           <p class="section-gallery__item__description" id="${id}">${categories} | ${release}</p>
@@ -22,7 +22,17 @@ function createGallary({
 }
 
 function generateContentGallery(data) {
-  return data.reduce((acc, item) => acc + createGallary(item), '');
+  // return data.reduce((acc, item) => acc + createGallary(item), '');
+  // Проверяем, находится ли пользователь на странице "Home" или "My Library"
+  const isHome = mainRefs.galleryList.id === 'home';
+
+  return data.reduce((acc, item) => {
+    // Добавляем или удаляем класс "paginated-list" в зависимости от текущей страницы
+    const listItemClass = isHome
+      ? 'section-gallery__item paginated-list'
+      : 'section-gallery__item';
+    return acc + createGallary(item, listItemClass);
+  }, '');
 }
 
 export { generateContentGallery };
